f20a19273addc8da0399c3ad712c84d5aa825b89
Collins M <collinsmensah4478@outlook.com>
2024-09-04 21:15:06 -0400

    Add files via upload

---
+++ Analysis Questions.sql
@@ -0,0 +1,10 @@
+/*
+1. How do the frequency and average amount of claims vary by month?
+2. What is the gender distribution among customers?
+3. Which state has the highest number of customers?
+4. Which customer has filed the highest number of claims?
+5. How do the claim amounts and frequency of claims differ across various policy types?
+6. How does the number of claims and claim amounts vary by state
+7. What is the distribution of claim statuses (e.g., approved, pending, rejected) over time?
+8. Which type of claim is most frequently reported?
+9. How do the number of claims filed vary among different customer age groups?
