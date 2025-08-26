19b16cce5b4c10dfd2ba5b291923f6c04b13f468
Collins <collinsmensah4478@outlook.com>
2025-08-26 14:29:28 -0400

    Add files via upload

---
+++ Analysis Questions.sql
@@ -0,0 +1,13 @@
+-- Analysis Questions
+
+/*
+1. How do the number of insurance claims and their average amounts change across different months?
+2. What is the breakdown of customers by gender?
+3. Which state contains the largest customer base?
+4. Identify the customer who has submitted the most claims.
+5. How do claim amounts and frequency of claims differ across various policy types?
+6. How does the number of claims and claim amounts vary by state?
+7. How do claim statuses (approved, pending, rejected) vary over time?
+8. Determine the claim type with the highest frequency.
+9. How does claim frequency differ across customer age groups?
+*/
